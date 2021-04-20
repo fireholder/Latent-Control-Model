9c8be6051499affa3190d747a17a50867442adfb
xwt <xuwenting33@qq.com>
2021-04-20 20:31:12 +0800

    Add files via upload
    feat: revise model parameters(to origin) and data

--- main_lcm.py
+++ main_lcm.py
@@ -1,5 +1,5 @@
 import torch
-from LCM import LatentControlModel
+from lcm import LatentControlModel
 from arg_test import get_args
 import numpy as np
 import torch.optim as optim
@@ -17,10 +17,10 @@ if args.cuda and torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
 
 model = LatentControlModel(
-                num_grid = 2,
+                num_grid = 4,
                 num_stack = 1,
                 action_space_n = 1,
-                obs_size = 2,
+                obs_size = 84,
                 random_noise_frame = True,
                 epsilon = 1,
                 ob_bound = 0,
@@ -29,16 +29,31 @@ model = LatentControlModel(
             )
 
 model.to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.1, betas=(0.0,0.9))
+optimizer = optim.Adam(model.parameters(), lr=0.00001, betas=(0.0,0.9))
 
 sampled = {}
-sampled['states'] = torch.Tensor(np.array([[[0,0]],[[1,0]],[[0,1]],[[1,1]]]))
+
+x = np.zeros((84,84))
+x1 = x.copy()
+x1[:42,:42] = 1
+x2 = x.copy()
+x2[:42,42:] = 1
+x3 = x.copy()
+x3[:42,:] = 1
+sampled['states'] = torch.Tensor(np.array([x,x1,x2,x3]))
+sampled['next_states'] = torch.Tensor(np.array([x,x2,x,x2]))
+
+
+# sampled['states'] = torch.Tensor(np.array([[[0,0]],[[1,0]],[[0,1]],[[1,1]]]))
+# sampled['states'] = torch.Tensor(np.array([[[0,0,0,1],[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,1,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]]))
 sampled['states'] = torch.unsqueeze(sampled['states'],1)
 sampled['states'] = Variable(sampled['states'], requires_grad=True).to(device)
-sampled['next_states'] = torch.Tensor(np.array([[[0,0]],[[0,1]],[[0,0]],[[0,1]]]))
+# sampled['next_states'] = torch.Tensor(np.array([[[0.1,0.1]],[[0.1,0.9]],[[0.1,0.1]],[[0.1,0.9]]]))
+# sampled['next_states'] = torch.Tensor(np.array([[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],[[0,0,0,1],[0,0,0,0],[0,0,0,0],[0,0,0,0]]]))
 sampled['next_states'] = torch.unsqueeze(sampled['next_states'],1)
 sampled['next_states'] = Variable(sampled['next_states'], requires_grad=True).to(device)
 sampled['actions'] = torch.Tensor(np.array([[0],[0],[0],[0]]))
+# sampled['actions'] = torch.Tensor(np.array([[0],[0]]))
 sampled['actions'] = Variable(sampled['actions'], requires_grad=True).to(device)
 # sampled['states'] = sampled['states'].repeat(10,1,1,1)
 # sampled['next_states'] = sampled['next_states'].repeat(10,1,1,1)
@@ -64,6 +79,7 @@ while True:
     writer.add_scalar('Train/Loss',loss.item(), epoch)
     writer.flush()
     loss.backward(retain_graph=True)
+    # ipdb.set_trace()
     '''optimize'''
     optimizer.step()
     epoch += 1
